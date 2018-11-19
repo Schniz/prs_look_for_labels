eda1ae766df7653917806b474e63a108b4878895
Gal Schlezinger <gal@spitfire.co.il>
2018-11-19 18:33:39 +0200

    support token param

--- src/prs_look_for_labels.cr
+++ src/prs_look_for_labels.cr
@@ -3,14 +3,16 @@ require "http"
 require "./markdown_to_html"
 require "./models"
 
-def create_status(pull_request : Models::PullRequest, payload)
+def create_status(pull_request : Models::PullRequest, payload, token)
+  url = "#{pull_request.statuses_url}?access_token=#{token}"
   headers = HTTP::Headers.new
   headers["Content-Type"] = "application/json"
   body = {state: payload[:state], description: payload[:description], context: "pr/labels"}.to_json
-  HTTP::Client.post(pull_request.statuses_url, headers: headers, body: body)
+  HTTP::Client.post(url, headers: headers, body: body)
 end
 
 post "/handle_pull_request" do |env|
+  token = env.params.query["token"]
   pull_request = Models::PullRequestPayload.from_json(env.request.body.not_nil!).pull_request
   payload =
     if pull_request.valid?
@@ -19,7 +21,7 @@ post "/handle_pull_request" do |env|
     else
       {state: :pending, description: "Please add `PR: ` labels to the pull request"}
     end
-  create_status(pull_request, payload)
+  create_status(pull_request, payload, token)
 end
 
 get "/" do
